7463ceecc6be3f782bb86898d96b612a683c1a7b
Foli <gf@guofang.me>
2018-07-11 16:56:25 +0800

    添加pdo单例模式私有化方法

--- drivers/Pdo.php
+++ drivers/Pdo.php
@@ -3,10 +3,13 @@ namespace YegoPHP\drivers;
 
 /**
  * 数据库驱动类
+ * @see 单例模式(三私一公)
  * @author Foli
  * @date 2018-4-3
  */
 class Pdo{
+    private function __construct(){} //禁止外部实例化
+    private function __clone(){} //防止对象被克隆
     private static $_pdo = NULL;
     /**
      * 单例模式返回PDO连接对象
